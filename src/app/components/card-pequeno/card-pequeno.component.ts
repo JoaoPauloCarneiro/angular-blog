2ba8910c19d49f335cbdde5bbd8ef7868e40af48
JoaoPauuloCarneiro <joopaulocarneiro85@gmail.com>
2023-10-17 20:52:18 -0300

    components dinamycs propertys

--- src/app/components/card-pequeno/card-pequeno.component.ts
+++ src/app/components/card-pequeno/card-pequeno.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-pequeno',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-pequeno.component.css']
 })
 export class CardPequenoComponent implements OnInit {
+  @Input ()
+  fotoCoverpequeno: string = ""
+  @Input ()
+  cardpequenotitle: string = ""
 
   constructor() { }
 
